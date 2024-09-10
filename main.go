1f128b4346f1a27ae49f7226001c84c4e1bb5f7f
Furkan Arslan <70799617+FFurkanArslan@users.noreply.github.com>
2024-09-10 22:01:25 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"github.com/ichtrojan/go-todo/routes"
 	"github.com/ichtrojan/thoth"
-	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
@@ -13,16 +12,11 @@ import (
 func main() {
 	logger, _ := thoth.Init("log")
 
-	if err := godotenv.Load(); err != nil {
-		logger.Log(errors.New("no .env file found"))
-		log.Fatal("No .env file found")
-	}
+	port := os.Getenv("PORT")
 
-	port, exist := os.LookupEnv("PORT")
-
-	if !exist {
-		logger.Log(errors.New("PORT not set in .env"))
-		log.Fatal("PORT not set in .env")
+	if port == "" {
+		logger.Log(errors.New("PORT not set in environment"))
+		log.Fatal("PORT not set in environment")
 	}
 
 	err := http.ListenAndServe(":"+port, routes.Init())
