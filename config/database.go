85b297e37ea7f9702d620b2488da284d4f38e02a
Furkan Arslan <70799617+FFurkanArslan@users.noreply.github.com>
2024-09-10 22:13:53 +0300

    Update database.go

--- config/database.go
+++ config/database.go
@@ -35,41 +35,42 @@ func Database() *sql.DB {
 		log.Fatal("DB_HOST not set in .env")
 	}
 
-	credentials := fmt.Sprintf("%s:%s@(%s:3306)/?charset=utf8&parseTime=True", user, pass, host)
+	 credentials := fmt.Sprintf("%s:%s@(%s:3306)/?charset=utf8&parseTime=True", user, pass, host)
 
-	database, err := sql.Open("mysql", credentials)
+    database, err := sql.Open("mysql", credentials)
+    if err != nil {
+        logger.Log(err)
+        log.Fatal(err)
+    }
 
-	if err != nil {
-		logger.Log(err)
-		log.Fatal(err)
-	} else {
-		fmt.Println("Database Connection Successful")
-	}
+    logger.Log(errors.New("Database Connection Successful"))
 
-	_, err = database.Exec(`CREATE DATABASE gotodo`)
+    // Check if database exists
+    _, err = database.Exec("CREATE DATABASE IF NOT EXISTS gotodo")
+    if err != nil {
+        logger.Log(err)
+        log.Fatal(err)
+    }
 
-	if err != nil {
-		fmt.Println(err)
-	}
+    _, err = database.Exec("USE gotodo")
+    if err != nil {
+        logger.Log(err)
+        log.Fatal(err)
+    }
 
-	_, err = database.Exec(`USE gotodo`)
+    // Check if table exists
+    _, err = database.Exec(`
+        CREATE TABLE IF NOT EXISTS todos (
+            id INT AUTO_INCREMENT,
+            item TEXT NOT NULL,
+            completed BOOLEAN DEFAULT FALSE,
+            PRIMARY KEY (id)
+        )
+    `)
+    if err != nil {
+        logger.Log(err)
+        log.Fatal(err)
+    }
 
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	_, err = database.Exec(`
-		CREATE TABLE todos (
-		    id INT AUTO_INCREMENT,
-		    item TEXT NOT NULL,
-		    completed BOOLEAN DEFAULT FALSE,
-		    PRIMARY KEY (id)
-		);
-	`)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return database
+    return database
 }
