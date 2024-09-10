d7105c2e872947d68639bf6cd5a2c2ba1f848c72
Furkan Arslan <70799617+FFurkanArslan@users.noreply.github.com>
2024-09-10 22:24:33 +0300

    Update database.go

--- config/database.go
+++ config/database.go
@@ -12,65 +12,29 @@ import (
 )
 
 func Database() *sql.DB {
-	logger, _ := thoth.Init("log")
+    logger, _ := thoth.Init("log")
 
-	user, exist := os.LookupEnv("DB_USER")
+    user := os.Getenv("DB_USER")
+    pass := os.Getenv("DB_PASS")
+    host := os.Getenv("DB_HOST")
+    dbName := os.Getenv("DB_NAME")
 
-	if !exist {
-		logger.Log(errors.New("DB_USER not set in .env"))
-		log.Fatal("DB_USER not set in .env")
-	}
-
-	pass, exist := os.LookupEnv("DB_PASS")
-
-	if !exist {
-		logger.Log(errors.New("DB_PASS not set in .env"))
-		log.Fatal("DB_PASS not set in .env")
-	}
-
-	host, exist := os.LookupEnv("DB_HOST")
-
-	if !exist {
-		logger.Log(errors.New("DB_HOST not set in .env"))
-		log.Fatal("DB_HOST not set in .env")
-	}
-
-	 credentials := fmt.Sprintf("%s:%s@(%s:3306)/?charset=utf8&parseTime=True", user, pass, host)
+    credentials := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8&parseTime=True", user, pass, host, dbName)
+    
+    logger.Log(errors.New(fmt.Sprintf("Attempting to connect to database at %s", host)))
 
     database, err := sql.Open("mysql", credentials)
     if err != nil {
-        logger.Log(err)
-        log.Fatal(err)
+        logger.Log(errors.New(fmt.Sprintf("Failed to open database: %v", err)))
+        return nil
+    }
+
+    err = database.Ping()
+    if err != nil {
+        logger.Log(errors.New(fmt.Sprintf("Failed to ping database: %v", err)))
+        return nil
     }
 
     logger.Log(errors.New("Database Connection Successful"))
-
-    // Check if database exists
-    _, err = database.Exec("CREATE DATABASE IF NOT EXISTS gotodo")
-    if err != nil {
-        logger.Log(err)
-        log.Fatal(err)
-    }
-
-    _, err = database.Exec("USE gotodo")
-    if err != nil {
-        logger.Log(err)
-        log.Fatal(err)
-    }
-
-    // Check if table exists
-    _, err = database.Exec(`
-        CREATE TABLE IF NOT EXISTS todos (
-            id INT AUTO_INCREMENT,
-            item TEXT NOT NULL,
-            completed BOOLEAN DEFAULT FALSE,
-            PRIMARY KEY (id)
-        )
-    `)
-    if err != nil {
-        logger.Log(err)
-        log.Fatal(err)
-    }
-
     return database
 }
